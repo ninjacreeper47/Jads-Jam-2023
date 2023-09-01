19844345601410e596c151b425976c72df21fc47
Zach morris <ninjacreeper47@gmail.com>
2023-09-01 16:30:21 -0500

    Added another print statement

--- test.gd
+++ test.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Hello World")
-
+	print("This message only appears in the cool hello-world with graphics branch")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
